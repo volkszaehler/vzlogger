c6868680018f5c118ab34c9697a0fb6a9e6e70c1
Kai Krueger <kai.krueger@itwm.fraunhofer.de>
2012-04-05 09:00:00 +0200

    Indentation

--- include/channel.h
+++ include/channel.h
@@ -37,70 +37,70 @@
 
 class Channel {
 
-public:
+	public:
 	typedef vz::shared_ptr<Channel> Ptr;
 
 	Channel(const std::list<Option> &pOptions, const std::string api, const std::string pUuid, ReadingIdentifier::Ptr pIdentifier);
 	virtual ~Channel();
 
-  void start() {
-    pthread_create(&_thread, NULL, &logging_thread, (void *) this);
-  }
-  
-  void join() {
-    pthread_join(_thread, NULL);
-  }
+	void start() {
+		pthread_create(&_thread, NULL, &logging_thread, (void *) this);
+	}
+	
+	void join() {
+		pthread_join(_thread, NULL);
+	}
 
-  void cancel() { pthread_cancel(_thread); }
+	void cancel() { pthread_cancel(_thread); }
 
-  const char* name()                  { return _name.c_str(); }
-  std::list<Option> &options()        { return _options; }
+	const char* name()                  { return _name.c_str(); }
+	std::list<Option> &options()        { return _options; }
 
-  ReadingIdentifier::Ptr identifier() {
-    if(_identifier.use_count() < 1) throw vz::VZException("Destroyed") ; return _identifier; }
-  const double tvtod() const          { return _last == NULL ? 0 : _last->tvtod(); }
-  
-  const char* uuid()                  { return _uuid.c_str(); }
-  const std::string apiProtocol()     { return _apiProtocol; }
+	ReadingIdentifier::Ptr identifier() {
+		if(_identifier.use_count() < 1) throw vz::VZException("Destroyed") ; return _identifier; }
+	const double tvtod() const          { return _last == NULL ? 0 : _last->tvtod(); }
+	
+	const char* uuid()                  { return _uuid.c_str(); }
+	const std::string apiProtocol()     { return _apiProtocol; }
 
-  void last(Reading *rd)              { _last = rd;}
-  void push(const Reading &rd)        { _buffer->push(rd); }
+	void last(Reading *rd)              { _last = rd;}
+	void push(const Reading &rd)        { _buffer->push(rd); }
 	char *dump(char *dump, size_t len)  { return _buffer->dump(dump, len); }
-  Buffer::Ptr buffer()                { return _buffer; }
-  
-  const size_t size() { return _buffer->size(); }  
-  const size_t keep() { return _buffer->keep(); }  
+	Buffer::Ptr buffer()                { return _buffer; }
+	
+	const size_t size() { return _buffer->size(); }  
+	const size_t keep() { return _buffer->keep(); }  
 
-  inline void notify() {
-    _buffer->lock();
-    pthread_cond_broadcast(&condition);
-    _buffer->unlock();
-  }
-  inline void wait() {
-    _buffer->lock();
-    while(!_buffer->newValues() ) {
+	inline void notify() {
+		_buffer->lock();
+		pthread_cond_broadcast(&condition);
+		_buffer->unlock();
+	}
+	inline void wait() {
+		_buffer->lock();
+		while(!_buffer->newValues() ) {
 			_buffer->wait(&condition); /* sleep until new data has been read */
-    }
-    _buffer->clear_newValues();
-    _buffer->unlock();
-  }
-  
-  private:
+		}
+		_buffer->clear_newValues();
+		_buffer->unlock();
+	}
+	
+	private:
 	static int instances;
 	int id;		       		  /**< only for internal usage & debugging */
-  std::string _name;    /**< name of the channel */
-  std::list<Option> _options;
-  
+	std::string _name;    /**< name of the channel */
+	std::list<Option> _options;
+	
 	Buffer::Ptr _buffer;		/**< circular queue to buffer readings */
-  
+	
 	ReadingIdentifier::Ptr _identifier;	/* channel identifier (OBIS, string) */
 	Reading *_last;			/* most recent reading */
 
 	pthread_cond_t condition;	 /**< pthread syncronization to notify logging thread and local webserver */
 	pthread_t _thread;		     /**< pthread for asynchronus logging */
 
- 	std::string _uuid;		   	 /**< unique identifier for middleware */
-  std::string _apiProtocol;  /**< protocol of api to use for logging */
+	std::string _uuid;		   	 /**< unique identifier for middleware */
+	std::string _apiProtocol;  /**< protocol of api to use for logging */
 };
 
 #endif /* _CHANNEL_H_ */
