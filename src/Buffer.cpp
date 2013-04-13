3c7fb7107e642e93e9cb827ddeeb86b2256e2822
peterevertz <leo2@pec.homeip.net>
2013-04-13 22:59:12 +0200

    clean after aggregate

--- src/Buffer.cpp
+++ src/Buffer.cpp
@@ -77,6 +77,7 @@ void Buffer::aggregate() {
 			}
 		}
 		unlock();
+		clean();
 		return;
 	}
 	if(_aggmode == AVG) {
@@ -110,6 +111,7 @@ void Buffer::aggregate() {
 			}
 		}
 		unlock();
+		clean();
 		return;
 	}
 }
