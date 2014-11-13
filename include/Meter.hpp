a2c4f8b383af7d47c1538d5e9098a7dd82df3f30
andig <cpuidle@gmx.de>
2014-11-13 09:02:01 +0100

    Added <allowskip> option to ignore non-working meters on startup

--- include/Meter.hpp
+++ include/Meter.hpp
@@ -48,51 +48,52 @@ public:
 	int close();
 	size_t read(std::vector<Reading> &rds, size_t n);
 
-// setter
+	// setter
 	void interval(const int i) { _interval = i; }
 
-// getter
-	const char *name() const               { return _name.c_str(); }
-	bool isEnabled() const           { return _enable; }
+	// getter
+	const char *name() const { return _name.c_str(); }
+	bool isEnabled() const { return _enable; }
 
-	meter_protocol_t protocolId() const { return _protocol_id; } 
+	meter_protocol_t protocolId() const { return _protocol_id; }
 	vz::protocol::Protocol::Ptr protocol() const { return _protocol; }
 
 	ReadingIdentifier::Ptr identifier() const { return _identifier; }
 
-	int  interval() const            { return _interval; }
+	int interval() const { return _interval; }
+	int skip() const { return _skip; }
 
-	int aggtime() const 				{ return _aggtime; }
-	bool aggFixedInterval() const 				{ return _aggFixedInterval; }
+	int aggtime() const { return _aggtime; }
+	bool aggFixedInterval() const { return _aggFixedInterval; }
 
 private:
-	static int instances;                   /**< meter instance id (increasing counter) */
-	// bool _thread_running;   								/**< flag if thread is started */
+	static int instances;                   // meter instance id (increasing counter)
+	// bool _thread_running;   				// flag if thread is started
 
-	int id;                                 /**< meter id */
-	std::string _name;                      /**< meter name */
-	bool _enable;                           /**< true if meter is disabled (default) */
+	int id;                                 // meter id
+	std::string _name;                      // meter name
+	bool _enable;                           // true if meter is disabled (default)
 
-	meter_protocol_t _protocol_id;          /**< meter protocol id */
-	vz::protocol::Protocol::Ptr _protocol;  /**< meter protocol */
+	meter_protocol_t _protocol_id;          // meter protocol id
+	vz::protocol::Protocol::Ptr _protocol;  // meter protocol
 
 	ReadingIdentifier::Ptr _identifier;
 
-
 	int _interval;
+	bool _skip;
 
 	int _aggtime;
 	bool _aggFixedInterval;
 
-	std::vector<Channel> channels;          /**< channel for logging */
+	std::vector<Channel> channels;          // channel for logging
 };
 
 typedef struct {
 	const meter_protocol_t id;
-	const char *name;		/* short identifier for protocol */
-	const char *desc;		/* more detailed description */
-	const size_t max_readings;	/* how many readings can be read with 1 call */
-	const bool periodic;		/* does this meter has be triggered periodically? */
+	const char *name;						// short identifier for protocol
+	const char *desc;						// more detailed description
+	const size_t max_readings;				// how many readings can be read with 1 call
+	const bool periodic;					// does this meter has be triggered periodically?
 } meter_details_t;
 
 /**
