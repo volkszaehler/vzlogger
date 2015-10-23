5ece3c73dca63244064c1831e5c0f1dcdd664cfd
andig <cpuidle@gmx.de>
2015-10-23 10:59:40 +0200

    Add Swiss OBIS codes

--- src/Obis.cpp
+++ src/Obis.cpp
@@ -43,14 +43,15 @@
 
 //const Obis::aliases[] = {
 static obis_alias_t aliases[] = {
-//   A    B    C    D    E    F    alias		description
-//   ====================================================================
+//	A    B    C    D    E    F    alias		description
+//	====================================================================
 
-// general
+//	General
+
 	{Obis(  1,   0,   1,   7,  DC,  DC), "power",		"Wirkleistung  (Summe)"},
-	{Obis(  1,   0,  21,   7,  DC,  DC), "power-l1",		"Wirkleistung  (Phase 1)"},
-	{Obis(  1,   0,  41,   7,  DC,  DC), "power-l2",		"Wirkleistung  (Phase 2)"},
-	{Obis(  1,   0,  61,   7,  DC,  DC), "power-l3",		"Wirkleistung  (Phase 3)"},
+	{Obis(  1,   0,  21,   7,  DC,  DC), "power-l1",	"Wirkleistung  (Phase 1)"},
+	{Obis(  1,   0,  41,   7,  DC,  DC), "power-l2",	"Wirkleistung  (Phase 2)"},
+	{Obis(  1,   0,  61,   7,  DC,  DC), "power-l3",	"Wirkleistung  (Phase 3)"},
 
 	{Obis(  1,   0,  12,   7,  DC,  DC), "voltage",		"Spannung      (Mittelwert)"},
 	{Obis(  1,   0,  32,   7,  DC,  DC), "voltage-l1",	"Spannung      (Phase 1)"},
@@ -62,26 +63,24 @@ static obis_alias_t aliases[] = {
 	{Obis(  1,   0,  51,   7,  DC,  DC), "current-l2",	"Stromstaerke  (Phase 2)"},
 	{Obis(  1,   0,  71,   7,  DC,  DC), "current-l3",	"Stromstaerke  (Phase 3)"},
 
-	{Obis(  1,   0,  14,   7,   0,  DC), "frequency",		"Netzfrequenz"},
+	{Obis(  1,   0,  14,   7,   0,  DC), "frequency",	"Netzfrequenz"},
 	{Obis(  1,   0,  12,   7,   0,  DC), "powerfactor",	"Leistungsfaktor"},
 
 	{Obis(  0,   0,  96,   1,  DC,  DC), "device",		"Zaehler Seriennr."},
 	{Obis(  1,   0,  96,   5,   5,  DC), "status",		"Zaehler Status"},
 
-	{Obis(  1,   0,   1,   8,	DC, DC), "counter",		"Zaehlerstand Wirkleistung"},
-	{Obis(  1,   0,   2,   8,  DC, DC), "counter-out",	"Zaehlerstand Lieferg."},
+	{Obis(  1,   0,   1,   8,  DC,  DC), "counter",		"Zaehlerstand Wirkleistung"},
+	{Obis(  1,   0,   2,   8,  DC,  DC), "counter-out",	"Zaehlerstand Lieferg."},
 
-// Easymeter
-
-// ESYQ3B (Easymeter Q3B)
+//	Easymeter
+//	ESYQ3B (Easymeter Q3B)
 	{Obis(  1,   0,   1,   8,   1,  DC), "esy-counter-t1",	"Active Power Counter Tariff 1"},
 	{Obis(  1,   0,   1,   8,   2,  DC), "esy-counter-t2",	"Active Power Counter Tariff 2"},
-//{Obis(129, 129, 199, 130,   3,  DC), "",		""}, // ???
+//	{Obis(129, 129, 199, 130,   3,  DC), "",		""}, // ???
+//	ESYQ3D (Easymeter Q3D)
+//	{Obis(  0,   0,   0,   0,   0,  DC), "",		""}, // ???
 
-// ESYQ3D (Easymeter Q3D)
-//{Obis(  0,   0,   0,   0,   0,  DC), "",		""}, // ???
-
-// HAG eHZ010C_EHZ1WA02 (Hager eHz)
+//	HAG eHZ010C_EHZ1WA02 (Hager eHz)
 	{Obis(  1,   0,   0,   0,   0,  DC), "hag-id",		"Eigentumsnr."},
 	{Obis(  1,   0,  96,  50,   0,   0), "hag-status",	"Netz Status"},			// bitcodiert: Drehfeld, Anlaufschwelle, Energierichtung
 	{Obis(  1,   0,  96,  50,   0,   1), "hag-frequency",	"Netz Periode"},		// hexadezimal (Einheit 1/100 ms)
@@ -92,8 +91,13 @@ static obis_alias_t aliases[] = {
 	{Obis(  1,   0,  96,  50,   0,   6), "hag-check",		"Kontrollnr."},
 	{Obis(  1,   0,  96,  50,   0,   7), "hag-diag",		"Diagnose"},
 
-//{} stop condition for iterator
-	{Obis(  DC,   DC,  DC,  DC,   DC,   DC), NULL,		NULL},
+//	Schweizer VNBs lt https://github.com/volkszaehler/vzlogger/issues/205
+	{Obis(255, 255,  16,    8,  0, 255), "lg-counter-et", "Sum active energy (Total)"},
+	{Obis(255, 255,  16,    8,  1, 255), "lg-counter-ht", "Sum active energy (T1)"},
+	{Obis(255, 255,  16,    8,  2, 255), "lg-counter-lt", "Sum active energy (T2)"},
+
+//	Stop condition for iterator
+	{Obis( DC,  DC,  DC,   DC, DC,  DC), NULL, NULL},
 };
 
 
