8d1ad4c26c834d21e05c6a871afa7fcee747389b
r00t <r00t@constancy.org>
2024-01-05 22:40:37 +0100

    comment fixes, fixup 46cd8253

--- include/api/InfluxDB.hpp
+++ include/api/InfluxDB.hpp
@@ -72,9 +72,8 @@ class InfluxDB : public ApiIF {
 	std::list<Reading> _values;
 	CurlResponse::Ptr _response;
 
-	// Volatile
-	int64_t _last_timestamp; /**< remember last timestamp */
-	// duplicate support:
+	int64_t _last_timestamp; /* remember last timestamp */
+	// duplicates support:
 	Reading *_lastReadingSent;
 
 	typedef struct {
