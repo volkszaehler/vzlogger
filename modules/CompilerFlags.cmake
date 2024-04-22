948034584b61a74c086492f10a952e694e917a8c
Jo <jz-2017@heute-morgen.de>
2024-04-22 06:05:53 +0200

    Fixed passing of Debian hardening flags to gcc

--- modules/CompilerFlags.cmake
+++ modules/CompilerFlags.cmake
@@ -8,7 +8,7 @@ if(NOT WIN32)
     message(STATUS "using gcc compiler ${CMAKE_CXX_COMPILER_ID}")
     include (CheckCXXSourceCompiles)
 
-    set(CMAKE_CXX_FLAGS "${CXXFLAGS} -W -Wall -Wextra -Werror -Wnon-virtual-dtor -Wno-system-headers")
+    set(CMAKE_CXX_FLAGS "$ENV{CXXFLAGS} -W -Wall -Wextra -Werror -Wnon-virtual-dtor -Wno-system-headers")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-deprecated-declarations")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Winit-self -Wmissing-include-dirs -Wno-pragmas -Wredundant-decls")
