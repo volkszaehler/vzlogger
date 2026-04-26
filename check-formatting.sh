90b7b2e3755652e9083216258a7072de9c3662f6
claashk <claashk@gmx.de>
2026-04-26 10:35:55 +0200

    Updated git workflow with a more recent version of clang-format

--- check-formatting.sh
+++ check-formatting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -xe
 SRCDIRS=(include src tests)
-clang-format --version
-find "${SRCDIRS[@]}"  -type f -name '*.h' -o -name '*.cpp' -o -name '*.hpp' | xargs -I{} -P1 clang-format -i -style=file {}
+clang-format-20 --version
+find "${SRCDIRS[@]}"  -type f -name '*.h' -o -name '*.cpp' -o -name '*.hpp' | xargs -I{} -P1 clang-format-20 -i -style=file {}
 [[ -z "$(git status --porcelain "${SRCDIRS[@]}")" ]] || (git status; git diff; false)
